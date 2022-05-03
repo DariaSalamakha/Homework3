998c3e7bad45f40fa43860a09f1dd4841c050820
Daria Salamakha <daria.salamakha@gmail.com>
2022-05-03 11:47:06 +0300

    Upload full project.

---
+++ Homework3/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Homework3
+//
+//  Created by Daria Salamakha on 29.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var whiteButton: UIButton!
+    @IBOutlet weak var blackButton: UIButton!
+    
+
+
+    
+
+    
+    
+}
+
